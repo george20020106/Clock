81191b7a19249347a84791e2e8b279681b2b151d
george20020106 <116645746+george20020106@users.noreply.github.com>
2023-07-06 15:03:23 +0800

    Delete dom.js

--- dom.js
+++
@@ -1,8 +0,0 @@
-export let selectYear = document.querySelector("#year");
-export let selectMonth = document.querySelector("#month");
-export let selectDay = document.querySelector("#day");
-export let selectHour = document.querySelector("#hour")
-export let selectMin = document.querySelector("#min");
-export let spanHelp = document.querySelector("span");
-export let submitButton = document.querySelector("button");
-export let setTimeContainer = document.querySelector("#setTimeContainer");
